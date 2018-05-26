b23c6670a79219e65bbde5924d1a0cbeacfbadf5
cesar.meneses <cesar.meneses@ceiba.com.co>
2018-05-26 12:51:59 -0500

    Se agrega el video que explica como compilar y ejecutar una clase

---
+++ contenido/Compilar y ejecutar/Aplicacion.java
@@ -0,0 +1,7 @@
+public class Aplicacion {
+	
+	public static void main(String[] args){
+		System.out.println("HOLA");
+	}
+	
+}
---
+++ contenido/Compilar y ejecutar/Persona.java
@@ -0,0 +1,20 @@
+public class Persona{
+	
+	// esta es la edad de la persona
+	public int edad;
+	
+	/* 
+	esto es un comentario
+	* de multiples lineas
+	*/
+	public void setEdad(int nuevaEdad){
+		edad = nuevaEdad;
+	}
+
+	/**
+
+	*/
+	public int getEdad(){
+		return edad;
+	}
+}
