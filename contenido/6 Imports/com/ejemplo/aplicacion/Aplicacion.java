5121a69fea65b099e2f06671c6c385fd6a23d43e
cesar.meneses <cesar.meneses@ceiba.com.co>
2018-05-28 20:19:08 -0500

    Se agrega la explicacion de los imports en java

---
+++ contenido/6 Imports/com/ejemplo/aplicacion/Aplicacion.java
@@ -0,0 +1,11 @@
+package com.ejemplo.aplicacion;
+
+import com.ejemplo.model.Persona;
+
+public class Aplicacion {
+	
+	public static void main(String[] args){
+		System.out.println("HOLA");
+	}
+	
+}
